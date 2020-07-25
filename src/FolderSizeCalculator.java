fda8abd906387f9648de34bd26cd10bd163aed46
aleksandr <tolastoialeksandr@mail.ru>
2020-07-25 15:14:22 +0300

    copy

--- src/FolderSizeCalculator.java
+++ src/FolderSizeCalculator.java
@@ -3,16 +3,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
-public class FolderSizeCalculator extends RecursiveTask<Long> {
+public class FolderSizeCalculator extends RecursiveTask<Long>
+{
     private File folder;
-    private long sizeKB = 1024;
 
     public FolderSizeCalculator(File folder) {
         this.folder = folder;
     }
 
     @Override
-    protected Long compute() {
+    protected Long compute()
+    {
         if(folder.isFile()) {
             return folder.length();
         }
@@ -28,36 +29,11 @@ public class FolderSizeCalculator extends RecursiveTask<Long> {
             subTasks.add(task);
         }
 
-        for(FolderSizeCalculator task : subTasks) {
+        for(FolderSizeCalculator task : subTasks){
             sum += task.join();
         }
 
         return sum;
-    }
-
-    public String getHumanReadableSize(long size) {
-
-        if (size / sizeKB > 0) {
-            if (size / (sizeKB * sizeKB * sizeKB) >= 1) {
-                return (size/(sizeKB * sizeKB * sizeKB)) + " Gb";
-            } else if(size/(sizeKB * sizeKB) >= 1) {
-                return (size/(sizeKB * sizeKB)) + " Mb";
-            }
-            return size/sizeKB + " Kb";
-
-        } else {
-            return size + " b";
-        }
-    }
-
-    public long getSizeFromHumanReadable(String size) {
-
-        if (size.contains("b")) {
-            return Long.parseLong(size.split("b")[0]);
-
-        }
-
-        return 0;
     }
 
 }
